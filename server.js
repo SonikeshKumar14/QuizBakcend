4f394c44058d1fdb86074da78d014a07b38c4815
Sonikesh <91758635+SonikeshKumar14@users.noreply.github.com>
2023-09-05 23:48:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ require("dotenv").config({path: "./config.env"});
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
-// app.use((request, response, next) => {
-//   response.setHeader('Access-Control-Allow-Origin', process.env.FRONT_LINK);
-//   next();
-// });
+app.use((request, response, next) => {
+  response.setHeader('Access-Control-Allow-Origin', process.env.FRONT_LINK);
+  next();
+});
 app.use(require("./routes/quizzes"));
 app.use(require("./routes/ranking"));
 
 const dbo = require("./db/conn");
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+//   });
+// }
 
 app.listen(port, ()=> {
     dbo.connectToServer(function(err){
